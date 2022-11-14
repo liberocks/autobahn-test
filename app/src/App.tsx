5d76047f98d9a33b64ac2da82e6340cf55c4ef56
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-14 08:05:19 +0700

    add react qurty

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './App.css';
 import { router } from './route';
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
   return (
     <RecoilRoot>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 };
